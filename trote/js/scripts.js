f519e7d0be8e4c817af956e044b6f0ff9f568a19
Gabriel Bortoloti <gabrielbortoloti2016@gmail.com>
2025-03-26 21:32:18 -0300

    trote

---
+++ trote/js/scripts.js
@@ -0,0 +1,12 @@
+function calcular (){
+    // recuperar o valor da ação social digitado
+    // decçaração variavel sem tipo
+    let acaoSocial = document.getElementById("acaoSocial").value
+    // recupera o valor da homenagem digitado
+    let homenagem = document.getElementById("homenagem").value
+    // vamos somar os valores
+    let soma = Number(acaoSocial) + Number(homenagem)
+    // devolve o resultado para html
+    document.getElementById("soma").innerHTML = soma.toFixed(2)
+    
+}
